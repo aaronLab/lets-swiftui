c6e817eb823ee948576fc7da5b96b9858b0898f4
Aaron Lee <aaronlab.net@gmail.com>
2021-01-07 22:01:58 +0900

    Init HelloSwiftUI

---
+++ 01/HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Aaron Lee on 2021-01-07.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
